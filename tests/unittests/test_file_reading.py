eb95b7866093a469ad2c154677409c338d6d22cf
EverydayQA <gang.liang.2011@gmail.com>
2019-02-05 20:38:27 -0500

    add fake tests

---
+++ tests/unittests/test_file_reading.py
@@ -0,0 +1,38 @@
+import unittest
+
+
+class TestFileReading(unittest.TestCase):
+    """
+    Test file_reading.py
+    """
+
+    def test_get_path(self):
+        # get_path(fname):
+        pass
+
+    def test_get_filename(self):
+        # (fname, remove_trailing_ftype=False)
+        pass
+
+    def test_mkdir(self):
+        pass
+
+    def test_load_nifti(self):
+        pass
+
+    def test_save_nifti(self):
+        # (fname, data, affine=None, header=None)
+        pass
+
+    def test_load_h5(self):
+        pass
+
+    def test_save_h5(self):
+        # (fname, data, compress=1, fast_compression=1):
+        pass
+
+    def test_save_text(self):
+        pass
+
+    def test_load_file(self):
+        pass
